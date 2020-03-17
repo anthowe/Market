6ef8a80d4e4e222378348c0c298b200985350f9e
Anthony Howe <anthowe33@gmail.com>
2020-03-17 16:33:09 -0700

    First Commit

---
+++ Market/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Market
+//
+//  Created by Anthony Howe on 3/16/20.
+//  Copyright © 2020 Anthony Howe. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
